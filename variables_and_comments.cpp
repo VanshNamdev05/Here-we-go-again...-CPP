52c3e633ef65933a6559eba402ed6bb231f04bf7
VanshNamdev05 <159474384+VanshNamdev05@users.noreply.github.com>
2024-08-26 11:10:38 +0530

    Add files via upload

---
+++ variables_and_comments.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //this program was made by vansh --> comment
+    cout<<"Hello World"<<endl;
+    int age = 19; 
+
+    ;cout<<age;
+
+    return 0;
+}

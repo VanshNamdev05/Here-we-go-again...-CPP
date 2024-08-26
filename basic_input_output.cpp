234862dc2c71e843b5f585731e65a5c2d653e816
VanshNamdev05 <159474384+VanshNamdev05@users.noreply.github.com>
2024-08-26 11:45:57 +0530

    Add files via upload

---
+++ basic_input_output.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int n1,n2;
+
+    cout<<"Enter the value of number 1 : ";
+    cin>>n1;
+    cout<<"Enter the value of number 2 : ";
+    cin>>n2;
+
+    cout<<"The value of number 1 is : "<<n1<<" and The value of number 2 is : "<<n2;
+    cout<<"\nThe sum is :"<<n1+n2;
+
+    return 0;
+}
